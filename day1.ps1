08dbe61b452ea24fa64b57df81d2c0f9e484d02c
Daniel M <self@mrmatthews.me.uk>
2019-03-03 10:16:41 +0000

    init

---
+++ day1.ps1
@@ -0,0 +1,27 @@
+﻿cls
+$content = get-content "./day1.txt"
+$value = [int]0
+$content | foreach{$value += [int]$_ }
+$value
+pause
+
+#
+#part 2
+#
+cls
+$value = [int]0
+$values = @()
+$done = $false
+while(!$done){
+    foreach($operation in $content){
+        $value += [int]$operation
+        if(!$values.Contains($value)){
+            $values+= $value
+        }else{
+        $done = $true;
+        break;
+        }
+    }
+}
+cls
+$value
---
+++ day2.ps1
@@ -0,0 +1,50 @@
+﻿$lines = get-content "./day2.txt"
+$twos = [int]0;
+$threes = [int]0;
+cls
+foreach($line in $lines){
+    $characters = $line[0..$line.Length]
+    $hasTwo = $false;
+    $hasThree = $false;
+    foreach($character in $characters){
+        $matches = ($characters.Where({$_ -eq $character})).Count
+        if($matches -eq 2)  {$hasTwo = $true}
+        if($matches -eq 3)  {$hasThree = $true}
+    }
+    if($hasTwo){$twos++}
+    if($hasThree){$threes++}
+}
+cls
+$twos
+$threes
+$twos * $threes
+
+pause
+
+#
+# parts 2
+#
+cls
+
+
+foreach($line in $lines)
+{
+    $currentLine = $line[0..$line.Length]
+    foreach($_line in $lines)
+    {
+        $lineToCheck = $_line[0..$_line.Length]
+        $answer = $()
+        for($i = 0; $i -lt $currentLine.Length; $i++){
+            if($currentLine[$i] -eq $lineToCheck[$i]){
+                $answer += $currentLine[$i]
+            }
+        }
+        if($answer.Length -eq 25){
+            $answer
+            pause
+        }
+    }
+}
+
+
+
